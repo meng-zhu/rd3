613b1a07176a6bdf46a3b0ed24bc8e90a63788f8
A_bo <a0939533183@gmail.com>
2016-08-22 07:23:22 +0000

    10*10踩地雷

---
+++ boom3.php
@@ -0,0 +1,140 @@
+<?php
+    //題目10*10 40M
+
+    /**
+     *  產生地雷位置(40個)
+     */
+    for($i = 0; $i < 40; $i++)
+    {
+        $m = rand(0,99);
+        for($j = 0; $j < count($boom); $j++)
+        {
+            if($boom[$j] === $m){
+                $i--;
+                continue 2;
+            }
+        }
+        $boom[] = $m;
+    }
+
+    /**
+     *  建立原始陣列
+     */
+    for($x = 0; $x < 10; $x++){
+        for($y = 0; $y < 10; $y++)
+        {
+            $place[$x][$y] = "?";
+        }
+    }
+
+    // echo count($place)."<br>".count($place[0]);
+    // foreach($place as $key)
+    // {
+    //     foreach($key as $value){
+    //         echo $value;
+    //     }echo "<br>";
+    // }
+
+    /**
+     * 把m塞到自己的位置
+     */
+    for($i = 0; $i < 40; $i++)
+    {
+        $temp = $boom[$i];
+        $x = floor($temp/10);
+        $y = $temp % 10;
+        $place[$x][$y] = "M";
+    }
+
+    // echo "<hr>";
+    // foreach($place as $key){
+    //     foreach($key as $value)
+    //     {
+    //         echo $value." ";
+    //     }
+    //     echo "<br>";
+    // }
+
+    for($x = 0; $x < 10; $x++){
+        for($y = 0; $y < 10; $y++)
+        {
+            if($place[$x][$y] === "?")
+            {
+                $num = 0;
+
+                //左上
+                if($place[$x-1][$y-1] === "M")
+                {
+                    $num++;
+                }
+                //上
+                if($place[$x-1][$y] === "M")
+                {
+                    $num++;
+                }
+                //右上
+                if($place[$x-1][$y+1] === "M")
+                {
+                    $num++;
+                }
+                //左
+                if($place[$x][$y-1] === "M")
+                {
+                    $num++;
+                }
+                //右
+                if($place[$x][$y+1] === "M")
+                {
+                    $num++;
+                }
+                //左下
+                if($place[$x+1][$y-1] === "M")
+                {
+                    $num++;
+                }
+                //下
+                if($place[$x+1][$y] === "M")
+                {
+                    $num++;
+                }
+                //右下
+                if($place[$x+1][$y+1] === "M")
+                {
+                    $num++;
+                }
+
+                $place[$x][$y] = $num;
+            }
+        }
+    }
+
+    // echo "<hr>";
+    // foreach($place as $key){
+    //     foreach($key as $value)
+    //     {
+    //         echo $value." ";
+    //     }
+    //     echo "<br>";
+    // }
+
+  /**
+    * 轉字串
+    */
+    for($x = 0; $x < 10; $x++){
+        for($y = 0; $y < 10; $y++)
+        {
+            $final = $final.$place[$x][$y];
+            if($y === 9)
+            {
+                if($x != 9){
+                    $final = $final."N";
+                }
+            }
+        }
+    }
+
+    // echo "<hr>";
+    echo $final;
+
+
+
